5407ab1a5b4c50d8bfc04204715bea41282e9a16
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-05-31 11:05:43 -0500

    Add: confusing-number - submission-7

---
+++ Data Structures & Algorithms/confusing-number/submission-7.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean confusingNumber(int n) {
+        boolean res = false;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        map.put(1, 1);
+        map.put(6, 9); 
+        map.put(8, 8);
+        map.put(9, 6);
+
+
+        int num = n;
+        int rem = 0;
+        while(num > 0) {
+            int temp = num % 10;
+            System.out.println(" Actual " + temp);
+            if(map.containsKey(temp)) {
+                temp = map.get(temp);
+                System.out.println(" Map Value" + temp);
+            } else {
+                return false;
+            }
+
+            rem = rem * 10 + temp;
+            System.out.println("Process" + rem);
+            num = num / 10;
+        }
+
+
+System.out.println("Final" + rem);
+        
+
+        return rem != n;
+    }
+}
