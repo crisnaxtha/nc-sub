9f67455e63d020e020a255d41b69194658c4981f
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-08-05 17:26:52 -0500

    Add: maximum-score-after-splitting-a-string - submission-6

---
+++ Data Structures & Algorithms/maximum-score-after-splitting-a-string/submission-6.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxScore(String s) {
+        int res = Integer.MIN_VALUE;
+
+        for(int i = 0; i < s.length() -1; i++) {
+            String left = s.substring(0, i + 1);
+
+            String right = s.substring(i + 1, s.length());
+            int l = 0;
+            int lc = 0;
+            while(l < left.length()) {
+                if(left.charAt(l) == '0') {
+                    lc++;
+                }
+                l++;
+            }
+            int r = 0;
+            int rc = 0;
+            while(r < right.length()) {
+                if(right.charAt(r) == '1') {
+                    rc++;
+                }
+                r++;
+            }
+
+            res = Math.max(res, lc + rc);
+        }
+
+        return res;
+    }
+}
