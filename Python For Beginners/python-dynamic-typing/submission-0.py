68cc7aad0cfd51b84b014489154e9ef2d7e90436
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-08-12 18:26:52 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 11.00
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Strin"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
