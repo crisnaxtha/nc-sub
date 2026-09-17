c546a239f912849523df9338b19c684837fb9967
Krishna Shrestha <krishnashrestha49@gmail.com>
2026-09-16 19:11:44 -0500

    Add: meeting-schedule - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule/submission-6.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals, Comparator.comparingInt(a -> a.start));
+
+        for(int i = 1; i < intervals.size(); i++) {
+            int start = intervals.get(i - 1).end;
+            int end = intervals.get(i).start;
+            if(start > end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
